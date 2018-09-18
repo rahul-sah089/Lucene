a2aff3b48b0d8aa6f3d64b9f95b060faf4d1d5e0
rahulsah089 <rahul.sah089@gmail.com>
2018-09-18 11:01:12 -0700

    lucuene basic eample

---
+++ LuceneBasics/src/com/lucene/LuceneConstant.java
@@ -0,0 +1,8 @@
+package com.lucene;
+//This class provide various constants to be used across the sample application
+public class LuceneConstant {
+	public static final String CONTENTS  = "contents";
+	public static final String FILE_NAME = "filename";
+	public static final String FILE_PATH = "filepath";
+	public static final int MAX_SEARCH = 10;
+}
